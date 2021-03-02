c2756439e0ee083d56948a76202b3895522a99de
Bethany Seeger <bseeger@jhu.edu>
2021-03-02 08:38:25 -0500

    Tests a few more scenarios in corporate body and person taxonomies

--- tests/10-migration-backend-tests/verification/verify_migrations_test.go
+++ tests/10-migration-backend-tests/verification/verify_migrations_test.go
@@ -25,69 +25,79 @@ const (
 	DrupalBaseurl = "https://islandora-idc.traefik.me"
 )
 
-// Verifies that the Person migrated by testcafe persons-01.csv and persons-02.csv match the expected fields and
-// values present in taxonomy-person-01.json
-func Test_VerifyTaxonomyTermPerson(t *testing.T) {
+// Verifies that the Person migrated by testcafe persons-01.csv and persons-02.csv 
+// match the expected fields and values present in taxonomy-person-01.json
+func Test_VerifyTaxonomyTermPerson_Person1(t *testing.T) {
+    verifyTaxonomyTermPerson(t, "taxonomy-person-01.json", "Ansel Easton");
+}
+
+// Verifies that the Person migrated by testcafe persons-01.csv and persons-02.csv 
+// match the expected fields and values present in taxonomy-person-01.json
+func Test_VerifyTaxonomyTermPerson_Person2(t *testing.T) {
+    verifyTaxonomyTermPerson(t, "taxonomy-person-02.json", "Lewis Wickes");
+}
+
+func verifyTaxonomyTermPerson(t *testing.T, fileName string, restOfName string) {
 	expectedJson := ExpectedPerson{}
-	unmarshalJson(t, "taxonomy-person-01.json", &expectedJson)
+    log.Printf("Test Person file: %s and %s", fileName, restOfName)
+    unmarshalJson(t, fileName, &expectedJson)
 
-	// sanity check the expected json
-	assert.Equal(t, "taxonomy_term", expectedJson.Type)
-	assert.Equal(t, "person", expectedJson.Bundle)
-	assert.Equal(t, "Ansel Easton", expectedJson.RestOfName[0])
+    // sanity check the expected json
+    assert.Equal(t, "taxonomy_term", expectedJson.Type)
+    assert.Equal(t, "person", expectedJson.Bundle)
+    assert.Equal(t, restOfName, expectedJson.RestOfName[0])
+    u := &JsonApiUrl{
+        t:            t,
+        baseUrl:      DrupalBaseurl,
+        drupalEntity: expectedJson.Type,
+        drupalBundle: expectedJson.Bundle,
+        filter:       "name",
+        value:        expectedJson.Name,
+    }
 
-	u := &JsonApiUrl{
-		t:            t,
-		baseUrl:      DrupalBaseurl,
-		drupalEntity: expectedJson.Type,
-		drupalBundle: expectedJson.Bundle,
-		filter:       "name",
-		value:        expectedJson.Name,
-	}
+    // retrieve json of the migrated entity from the jsonapi and unmarshal the single response
+    personRes := &JsonApiPerson{}
+    u.get(personRes)
 
-	// retrieve json of the migrated entity from the jsonapi and unmarshal the single response
-	personRes := &JsonApiPerson{}
-	u.get(personRes)
+    // for each field in expected json,
+    //   see if the expected field matches the actual field from retrieved json
+    //   resolve relationships if required
+    //     - required for schema:knows
+    actual := personRes.JsonApiData[0]
+    assert.Equal(t, expectedJson.Type, actual.Type.entity())
+    assert.Equal(t, expectedJson.Bundle, actual.Type.bundle())
+    assert.Equal(t, expectedJson.PrimaryName, actual.JsonApiAttributes.PrimaryPartOfName)
+    assert.ElementsMatch(t, expectedJson.RestOfName, actual.JsonApiAttributes.PreferredNameRest)
+    assert.ElementsMatch(t, expectedJson.Prefix, actual.JsonApiAttributes.PreferredNamePrefix)
+    assert.ElementsMatch(t, expectedJson.Suffix, actual.JsonApiAttributes.PreferredNameSuffix)
+    assert.ElementsMatch(t, expectedJson.Number, actual.JsonApiAttributes.PreferredNameNumber)
+    assert.ElementsMatch(t, expectedJson.AltName, actual.JsonApiAttributes.PersonAlternateName)
+    assert.ElementsMatch(t, expectedJson.Date, actual.JsonApiAttributes.Dates)
+    assert.Equal(t, expectedJson.Authority[0].Uri, actual.JsonApiAttributes.Authority[0].Uri)
+    assert.Equal(t, expectedJson.Authority[0].Type, actual.JsonApiAttributes.Authority[0].Source)
+    assert.True(t, len(actual.JsonApiAttributes.Description.Processed) > 0)
+    assert.Equal(t, expectedJson.Description.Processed, actual.JsonApiAttributes.Description.Processed)
+    assert.True(t, len(actual.JsonApiAttributes.Description.Value) > 0)
+    assert.Equal(t, expectedJson.Description.Value, actual.JsonApiAttributes.Description.Value)
+    assert.Equal(t, expectedJson.Description.Format, actual.JsonApiAttributes.Description.Format)
 
-	// for each field in expected json,
-	//   see if the expected field matches the actual field from retrieved json
-	//   resolve relationships if required
-	//     - required for schema:knows
-	actual := personRes.JsonApiData[0]
-	assert.Equal(t, expectedJson.Type, actual.Type.entity())
-	assert.Equal(t, expectedJson.Bundle, actual.Type.bundle())
-	assert.Equal(t, expectedJson.PrimaryName, actual.JsonApiAttributes.PrimaryPartOfName)
-	assert.ElementsMatch(t, expectedJson.RestOfName, actual.JsonApiAttributes.PreferredNameRest)
-	assert.ElementsMatch(t, expectedJson.Prefix, actual.JsonApiAttributes.PreferredNamePrefix)
-	assert.ElementsMatch(t, expectedJson.Suffix, actual.JsonApiAttributes.PreferredNameSuffix)
-	assert.ElementsMatch(t, expectedJson.Number, actual.JsonApiAttributes.PreferredNameNumber)
-	assert.ElementsMatch(t, expectedJson.AltName, actual.JsonApiAttributes.PersonAlternateName)
-	assert.ElementsMatch(t, expectedJson.Date, actual.JsonApiAttributes.Dates)
-	assert.Equal(t, expectedJson.Authority[0].Uri, actual.JsonApiAttributes.Authority[0].Uri)
-	assert.Equal(t, expectedJson.Authority[0].Type, actual.JsonApiAttributes.Authority[0].Source)
-	assert.True(t, len(actual.JsonApiAttributes.Description.Processed) > 0)
-	assert.Equal(t, expectedJson.Description.Processed, actual.JsonApiAttributes.Description.Processed)
-	assert.True(t, len(actual.JsonApiAttributes.Description.Value) > 0)
-	assert.Equal(t, expectedJson.Description.Value, actual.JsonApiAttributes.Description.Value)
-	assert.Equal(t, expectedJson.Description.Format, actual.JsonApiAttributes.Description.Format)
+    // Resolve relationship to a name
+    assert.Equal(t, 1, len(actual.JsonApiRelationships.Relationships.Data))
+    relData := actual.JsonApiRelationships.Relationships.Data[0]
+    assert.Equal(t, "schema:knows", relData.Meta["rel_type"])
+    u.value = expectedJson.Knows[0]
 
-	// Resolve relationship to a name
-	assert.Equal(t, 1, len(actual.JsonApiRelationships.Relationships.Data))
-	relData := actual.JsonApiRelationships.Relationships.Data[0]
-	assert.Equal(t, "schema:knows", relData.Meta["rel_type"])
-	u.value = expectedJson.Knows[0]
+    // retrieve json of the resolved entity from the jsonapi
+    personRes = &JsonApiPerson{}
+    u.get(personRes)
+    relSchemaKnows := personRes.JsonApiData[0]
 
-	// retrieve json of the resolved entity from the jsonapi
-	personRes = &JsonApiPerson{}
-	u.get(personRes)
-	relSchemaKnows := personRes.JsonApiData[0]
+    // sanity
+    assert.Equal(t, relSchemaKnows.Type.bundle(), "person")
+    assert.Equal(t, relSchemaKnows.Type.entity(), "taxonomy_term")
 
-	// sanity
-	assert.Equal(t, relSchemaKnows.Type.bundle(), "person")
-	assert.Equal(t, relSchemaKnows.Type.entity(), "taxonomy_term")
-
-	// test
-	assert.Equal(t, expectedJson.Knows[0], relSchemaKnows.JsonApiAttributes.Name)
+    // test
+    assert.Equal(t, expectedJson.Knows[0], relSchemaKnows.JsonApiAttributes.Name)
 }
 
 func Test_VerifyTaxonomyTermAccessRights(t *testing.T) {
